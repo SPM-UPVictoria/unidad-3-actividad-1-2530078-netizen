4b7227280d9fa4ca14fc317b6f4eff004b78a8bd
Jesus Roque <2530078@upv.edu.mx>
2026-08-09 22:21:41 -0600

    se agregaron los archivos base de la actividad

---
+++ AstreaFleet/src/main/java/core/AstreaException.java
@@ -0,0 +1,8 @@
+package core;
+
+public class AstreaException extends Exception{
+    public AstreaException (String mensaje){
+        super(mensaje);
+    }
+    
+}
---
+++ AstreaFleet/src/main/java/core/InterfazPropulsable.java
@@ -0,0 +1,9 @@
+package core;
+
+/**
+ * InterfazPropulsable
+ */
+public interface InterfazPropulsable {
+
+    void activarHiperViaje(double factorWarp) throws FallaSistemaException,CombustibleInsuficienteException;
+}
